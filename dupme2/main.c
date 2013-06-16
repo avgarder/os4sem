37b0eac407904f54c25d0f67f84c9ce3af354919
Anton Garder <avgarder@avgarder-osx.local>
2013-06-16 21:57:34 +0400

    Fixed main.c

--- dupme2/main.c
+++ dupme2/main.c
@@ -6,7 +6,10 @@
 const int STDIN = 0;
 const int STDOUT = 1;
 
-int get_pos_0(char* c, size_t from, size_t to) {
+const char* READ = "read failed";
+const char* WRITE = "write_failed";
+
+int find_n(char* c, size_t from, size_t to) {
 	for (; from < to; from++) {
 		if (*(c + from) == '\n') {
 			return from;
@@ -15,42 +18,52 @@ int get_pos_0(char* c, size_t from, size_t to) {
 	return -1;
 }
 
-void check_io(int res) {
-	if (res == -1) {
-		exit(31);
+void check_io(const char* comment, int what) {
+	if (what < 0) {
+		perror(comment);
+		_exit(1);
 	}
 }
 
 int main(int argc, char** argv) {
+	if (argc < 2) {
+		return 1;
+	}
 	size_t buffer_size = (size_t) atoi(argv[1]) + 1;
 	char* buffer = (char*) malloc(sizeof(char) * buffer_size);
 	size_t buffer_len = 0;
 	int r, w;
-	while (true) {
-		r = read(STDIN, buffer + buffer_len, buffer_size - buffer_len);
-		check_io(r);
-		if (r == 0) {
-			break;
+	int i;
+	while (1) {
+		int pos = find_n(buffer, 0, buffer_len);
+		if (pos == -1) {
+			r = read(STDIN, buffer + buffer_len, buffer_size - buffer_len);
+			check_io(READ, r);
+			if (r == 0) {
+				break;
+			}
+			pos = find_n(buffer, buffer_len, buffer_len + r);
 		}
-		int pos = get_pos_0(buffer, buffer_len, buffer_len + r);
 		buffer_len += r;
 		if (pos != -1) {
-			int writed = 0;
-			while (writed < pos) {
-				w = write(STDOUT, buffer + writed, pos - writed);
-				check_io(w);
-				writed += w;
+			for (i = 0; i < 2; i++) {
+				int writed = 0;
+				while (writed <= pos) {
+					w = write(STDOUT, buffer + writed, pos + 1 - writed);
+					check_io(WRITE, w);
+					writed += w;
+				}
 			}
 			memmove(buffer, buffer + pos + 1, buffer_len - pos - 1);
 			buffer_len = buffer_len - pos - 1;
 		} else if (buffer_len == buffer_size) {
 			while (pos == -1) {
 				r = read(STDIN, buffer, buffer_size);
-				check_io(r);
+				check_io(READ, r);
 				if (r == 0) {
 					break;
 				}
-				pos = get_pos_0(buffer, 0, r);
+				pos = find_n(buffer, 0, r);
 			}
 			if (r == 0) {
 				break;
